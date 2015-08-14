de74df88beec4c470a00c31ca843227bb6c399e7
Christine Cazares <3cccazares@gmail.com>
2015-08-14 14:37:22 -0500

    add bootsrip and jquery

--- finalReview/Problem2/problem2.js
+++ finalReview/Problem2/problem2.js
@@ -6,20 +6,20 @@ jQuery has been imported for you.
 A client has contacted you because they've written terrible JavaScript.
 Repair their code! There are 10 bugs in the code. Make a short description of what you've fixed.
 
-1.  ?
-2.  ?
-3.  ?
-4.  ?
-5.  ?
-6.  ?
-7.  ?
-8.  ?
-9.  ?
-10. ?
+1.  21 needs a $ in frount  
+2.  the end shound look like this (){
+3.  also document should look like this (document)
+4.  line 22 'nav <ul/>'
+5.  no css to get .on
+6.  needs to list style type 
+7.  <p/>
+8.  output
+9.  singlenght
+10. consol.log
 */
 
-document.ready(function {
-    $('nav .ul li').css('list-style-type' square);
+$(document).ready(function() {
+    $('nav <ul/>').on('list-style-type' square);
 
-    $('.output').text('.p'.text.split( ).length());
+    $('.output').text('<p/>'.text.split( ).length());
 });
--- finalReview/Problem5/problem5.js
+++ finalReview/Problem5/problem5.js
@@ -11,21 +11,44 @@ as well as be able to meow (function that prints something to console)
 /*
 Write the Cat object definition
 */
+function Cat(color, weight, meow) {
+    this.color = color;
+    this.weight = weight;
+    this.meow = meow;
+    this.setColor = function (color) {
+        this.color = color;
+        $("#color").text(this.color);
+    }
+    this.setWeight = function (weight) {
+        this.weight = weight;
+        $("#weight").text(this.weight);
+    }
+    this.setmeow = function (meow) {
+        this.meow = meow;
+        $("#meow").text(this.meow);
+    }
 
-$(document).ready(function() {
+}
+
+$(document).ready(function () {
     var cat = new Cat();
 
-    $('#colorPicker').on('click', function() {
+
+
+    $('#colorPicker').on('click', function () {
         cat.color = $('#color-input').val();
         $('.catBlend').css('background-color', cat.color);
     });
 
-    $('#weight').on('click', function() {
+
+    $('#weight').on('click', function () {
         cat.weight = $('#weight-input').val();
         $('#weight-output').text(cat.weight);
     });
-
-    $('.cat').on('click', function() {
+    setInterval(function () {
+        cat.setmeow(cat.moew + 1);
+    }, 1000);
+    $('.cat').on('click', function () {
         console.log("The " + cat.color + " cat weighs " + cat.weight + " pounds");
         cat.meow();
     })
